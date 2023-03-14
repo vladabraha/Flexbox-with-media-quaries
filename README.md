c25c4ed42df9cf7a0c1b0c4eafb18c8447f84067
vladabraha <36154909+vladabraha@users.noreply.github.com>
2023-03-14 15:04:23 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flexbox with media quaries example
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rezor12/pen/QWVmmBe](https://codepen.io/rezor12/pen/QWVmmBe).
+
---
+++ src/script.js
@@ -0,0 +1,13 @@
+/* CHALLENGE: 
+
+a. Place all 5 figures are on the same row with a 2% gap between each box. You should not need to change the HTML to do this. 
+
+b. If the window is less than 850px, have 3 boxes on one row and 2 boxes on the second row. Maintain the 2% gap between each box in the first row. (You'll have >2% gap on the 2nd row.) Distribute any gap as evenly as you can if it's greater than 2%.
+
+c. BONUS: 
+At smaller screen dimensions (<= 850px), have the boxes appear in the center of the page.
+
+ At >850px, align the single row of boxes at the bottom of the page using Flexbox-specific properties (i.e. not margin or positioning).
+
+*/
+
